3d9a54c6d0cf217b9d5a64fa0e0b6265365c1a91
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-09-03 17:52:46 -0500

    commit desafio git

---
+++ app.py
@@ -0,0 +1,4 @@
+nome=input ('Qual e o seu nome?')
+idade=input ('Qual e a sua idade?')
+programacao=input ('Qual e a sua linguagem de programacao favorita?')
+print (f"meu nome: {nome}\nminha idade: {idade}\nLinguagem de programacao: {programacao}".format(nome,idade,programacao))
