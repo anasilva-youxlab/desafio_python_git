ac7af06a07d3dfebbdb781603402a8deaae6af44
anasilva-youxlab <ana.silva-youxlab@institutoyoux.org.br>
2025-09-08 13:11:53 -0500

    mudanca no codigo

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 nome=input ('Qual e o seu nome?')
 idade=input ('Qual e a sua idade?')
 programacao=input ('Qual e a sua linguagem de programacao favorita?')
-print (f"meu nome: {nome}\nminha idade: {idade}\nLinguagem de programacao: {programacao}".format(nome,idade,programacao))
+print (f"meu nome: {nome}\nminha idade: {idade}\nLinguagem de programacao: {programacao}")
+idade= int(input('qual e a sua idade?' ))
